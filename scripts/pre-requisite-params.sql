2aabad6f8a9a446f6fc9a039fb85fa95a176c7bb
junkratroadhog <junkratroadhogana@gmail.com>
2025-09-04 09:50:29 +0530

    commit

--- scripts/pre-requisite-params.sql
+++ scripts/pre-requisite-params.sql
@@ -1,7 +1,6 @@
 WHENEVER SQLERROR EXIT FAILURE;
 
 PROMPT === Checking FORCE LOGGING status in CDB ===
-COLUMN FORCE_LOGGING_ENABLED FORMAT A10
 SELECT force_logging FROM v$database;
 
 PROMPT === Enabling FORCE LOGGING in CDB if not already enabled ===
@@ -14,12 +13,28 @@ BEGIN
     END IF;
 END;
 /
+SELECT force_logging AS FORCE_LOGGING_ENABLED FROM v$database;
+
+PROMPT === Checking Supplemental Log Data in CDB$ROOT ===
+SELECT supplemental_log_data_min FROM v$database;
+
+PROMPT === Enabling Supplemental Log Data in CDB$ROOT if not already enabled ===
+DECLARE
+    v_sup VARCHAR2(3);
+BEGIN
+    SELECT supplemental_log_data_min INTO v_sup FROM v$database;
+    IF v_sup = 'NO' THEN
+        EXECUTE IMMEDIATE 'ALTER DATABASE ADD SUPPLEMENTAL LOG DATA';
+    END IF;
+END;
+/
+SELECT supplemental_log_data_min AS SUP_LOG_ENABLED_CDB FROM v$database;
 
 PROMPT === Switching to PDB &1 ===
 ALTER SESSION SET CONTAINER=&1;
+SHOW CON_NAME;
 
-PROMPT === Checking Supplemental Log Data status in PDB ===
-COLUMN SUP_LOG_ENABLED FORMAT A10
+PROMPT === Checking Supplemental Log Data in PDB ===
 SELECT supplemental_log_data_min FROM v$database;
 
 PROMPT === Enabling Supplemental Log Data in PDB if not already enabled ===
@@ -32,4 +47,6 @@ BEGIN
     END IF;
 END;
 /
+SELECT supplemental_log_data_min AS SUP_LOG_ENABLED_PDB FROM v$database;
+
 EXIT;
