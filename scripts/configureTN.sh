112e7eee2f64552c5f182ba43897ebadeb226a87
junkratroadhog <junkratroadhogana@gmail.com>
2025-09-08 23:42:44 +0530

    commit

--- scripts/configureTN.sh
+++ scripts/configureTN.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -e
 
+# Required environment variables: OGG_HOME, STAGE_DIR, ORA_BASE (optional)
+: "${port_number:?Environment variable port_number must be set}"
+: "${OGG_DEPLOY_NAME:?Environment variable OGG_DEPLOY_NAME must be set}"
+: "${deploy_username:=oggadmin}"   # Default base if not provided
+: "${deploy_password:=oracle}" # Default inventory location
+
 export PATH=$OGG_HOME/bin:$PATH
 
 $OGG_HOME/bin/ServiceManager start
