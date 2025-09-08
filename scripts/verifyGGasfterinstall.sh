ed88f39272a777bc4c82d0f3e0e12544466c63e4
junkratroadhog <junkratroadhogana@gmail.com>
2025-09-08 07:04:10 +0530

    commit

--- scripts/verifyGGasfterinstall.sh
+++ scripts/verifyGGasfterinstall.sh
@@ -6,7 +6,8 @@ set -e
 : "${OGG_HOME:?Environment variable OGG_HOME must be set}"
 
 export PATH=$OGG_HOME/bin:$PATH
-if [ ! -x "\$OGG_HOME/bin/adminclient" ]; then
+
+if [ ! -x "$OGG_HOME/bin/adminclient" ]; then
   echo 'ERROR: adminclient not found in $OGG_HOME/bin'
   exit 1
 fi
