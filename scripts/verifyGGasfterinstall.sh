da64c7e32a1301ea007932778fed0f10cc18bbf5
junkratroadhog <junkratroadhogana@gmail.com>
2025-09-08 06:58:36 +0530

    commit

--- scripts/verifyGGasfterinstall.sh
+++ scripts/verifyGGasfterinstall.sh
@@ -6,8 +6,8 @@ set -e
 : "${OGG_HOME:?Environment variable OGG_HOME must be set}"
 
 export PATH=$OGG_HOME/bin:$PATH
-if [ ! -x "$OGG_HOME/bin/adminclient" ]; then
-  echo 'ERROR: adminclient not found in \$OGG_HOME/bin'
+if [ ! -x "\$OGG_HOME/bin/adminclient" ]; then
+  echo 'ERROR: adminclient not found in $OGG_HOME/bin'
   exit 1
 fi
 echo 'GoldenGate AdminClient Version:'
