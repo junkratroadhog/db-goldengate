aff9aa8f60e9d887cd1350fda8ae0e34a797189d
junkratroadhog <junkratroadhogana@gmail.com>
2025-09-09 12:52:53 +0530

    commit

--- scripts/create_deploy_rsp.sh
+++ scripts/create_deploy_rsp.sh
@@ -1,12 +1,14 @@
+#!/bin/bash
 set -e
 
-# Expecting values from environment variables
 : "${OGG_DEPLOY_NAME:?Environment variable OGG_DEPLOY_NAME must be set}"
 : "${DEPLOY_USERNAME:?Environment variable DEPLOY_USERNAME must be set}"
 : "${DEPLOY_PASSWORD:?Environment variable DEPLOY_PASSWORD must be set}"
 : "${SM_PORT:?Environment variable SM_PORT must be set}"
 : "${AM_PORT:?Environment variable AM_PORT must be set}"
 : "${OGG_HOME:?Environment variable OGG_HOME must be set}"
+
+mkdir -p ${OGG_HOME}/var
 
 cat > /tmp/install_scripts/ogg_deploy.rsp <<EOF
 DEPLOYMENT_NAME=${OGG_DEPLOY_NAME}
